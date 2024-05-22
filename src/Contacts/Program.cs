975d398096ae7a792373e72ae32805a46f92b486
mimovnik <mimovnik@protonmail.com>
2024-05-22 15:48:47 +0200

    Add cors policy

--- src/Contacts/Program.cs
+++ src/Contacts/Program.cs
@@ -8,10 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApi()
         .AddApplication()
         .AddPersistence();
+
+    if (builder.Environment.IsDevelopment())
+    {
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowLocalhost",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+        });
+    }
 }
 
 var app = builder.Build();
 {
+    if (builder.Environment.IsDevelopment())
+    {
+        app.UseCors("AllowLocalhost");
+    }
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
